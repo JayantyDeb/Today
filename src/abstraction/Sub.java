2bd7713e2ba86a643a6a0420ab9a77c0d1f6d3b4
Biplob Paul <Biplob Paul@10.67.225.40>
2021-11-13 15:09:47 -0500

    First time commit

---
+++ src/abstraction/Sub.java
@@ -0,0 +1,6 @@
+package abstraction;
+
+public interface Sub {
+	abstract void sub(int x, int y);
+
+}
---
+++ src/interm/SwitchCaseDemo.java
@@ -0,0 +1,22 @@
+package interm;
+
+public class SwitchCaseDemo {
+
+	public static void main(String[] args) {
+		int day;
+		day = 6;
+		switch (day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+		case 3:
+			System.out.println("Wednesday");
+		default:
+			System.out.println("Invalid Input");
+		}
+
+	}
+
+}
