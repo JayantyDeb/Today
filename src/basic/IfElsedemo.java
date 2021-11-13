413e0ef8e3dbbb902dd4fcf77a7d13a1796ba050
Biplob Paul <Biplob Paul@10.67.225.40>
2021-11-13 15:05:50 -0500

    put if else and swap

---
+++ src/basic/IfElsedemo.java
@@ -0,0 +1,28 @@
+package basic;
+
+public class IfElsedemo {
+
+	public static void main(String[] args) {
+		/*int x;
+		int y;
+		int sub;
+		x = 100;
+		y = 200;
+
+		if (x > y)
+			sub = x - y;
+		else
+			sub = y - x;
+		System.out.println(sub);*/
+		
+		double x;
+		double y;
+		double sum;
+		x=2.3;
+		y=5.55;
+		sum=x+y;
+		System.out.println(sum);
+
+	}
+
+}
---
+++ src/interm/SwapDemo.java
@@ -0,0 +1,21 @@
+package interm;
+
+public class SwapDemo {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int temp;
+		x=10;
+		y=5;
+		
+		temp=x;
+		x=y;
+		y=temp;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
